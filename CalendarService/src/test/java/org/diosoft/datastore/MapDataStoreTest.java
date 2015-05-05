579011a21dc01c9a15e059877c58771b9f06f1ee
alex-anakin <aleksandr.anakin@gmail.com>
2015-05-05 22:46:17 +0300

    add tests

---
+++ CalendarService/src/test/java/org/diosoft/datastore/MapDataStoreTest.java
@@ -0,0 +1,26 @@
+package org.diosoft.datastore;
+
+import org.junit.Test;
+
+public class MapDataStoreTest {
+
+    @Test
+    public void testAddEvent() {
+
+    }
+
+    @Test
+    public void testGetEvent() {
+
+    }
+
+    @Test
+    public void testGetAllEvents() {
+
+    }
+
+    @Test
+    public void testRemoveEvent() {
+
+    }
+}
---
+++ CalendarService/src/test/java/org/diosoft/service/CalendarServiceImplTest.java
@@ -0,0 +1,31 @@
+package org.diosoft.service;
+
+import org.junit.Test;
+
+public class CalendarServiceImplTest {
+
+    @Test
+    public void testAddEvent() {
+
+    }
+
+    @Test
+    public void testGetEvent() {
+
+    }
+
+    @Test
+    public void testGetAllEvents() {
+
+    }
+
+    @Test
+    public void testRemoveEvent() {
+
+    }
+
+    @Test
+    public void testMakeId() {
+
+    }
+}
